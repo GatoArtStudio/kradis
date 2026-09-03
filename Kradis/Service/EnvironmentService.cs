56ab368200393b869d2a5a030c5bc06fc9e3c60d
GatoArtStudio <hackhcctecno@gmail.com>
2026-09-02 23:20:09 -0500

    ref: These are constants that allow access from anywhere in the app to environment variables.

--- Kradis/Service/EnvironmentService.cs
+++ Kradis/Service/EnvironmentService.cs
@@ -4,14 +4,17 @@ namespace Kradis.Service;
 
 public class EnvironmentService
 {
+    public const string KeyDiscordApplicationToken = "DISCORD_APPLICATION_TOKEN";
+    public const string KeyDefaultConnectionStringMySql = "DEFAULT_CONNECTION_STRING_MYSQL";
+    
     public EnvironmentVariables EnvironmentVariables { get; init; }
 
     public EnvironmentService()
     {
         DotNetEnv.Env.Load();
 
-        string discordApplicationToken = Environment.GetEnvironmentVariable("DISCORD_APPLICATION_TOKEN") ?? throw new InvalidOperationException("DISCORD_APPLICATION_TOKEN is not set");
-        string? defaultConnectionStringMySql = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION_STRING_MYSQL");
+        string discordApplicationToken = Environment.GetEnvironmentVariable(KeyDiscordApplicationToken) ?? throw new InvalidOperationException($"{KeyDiscordApplicationToken} is not set");
+        string? defaultConnectionStringMySql = Environment.GetEnvironmentVariable(KeyDefaultConnectionStringMySql);
 
         EnvironmentVariables = new EnvironmentVariables(
             DiscordApplicationToken: discordApplicationToken,
