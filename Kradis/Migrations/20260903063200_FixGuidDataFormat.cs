02b75439cad57258d199d3169b9b876abd51947a
GatoArtStudio <hackhcctecno@gmail.com>
2026-09-03 01:44:40 -0500

    add: Migration to update the database with the correct data type for the guild ID.

---
+++ Kradis/Migrations/20260903063200_FixGuidDataFormat.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kradis.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGuidDataFormat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Id",
+                table: "guilds",
+                type: "binary(16)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "binary(36)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Id",
+                table: "guilds",
+                type: "binary(36)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "binary(16)");
+        }
+    }
+}
