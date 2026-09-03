a211c7ef433e5f6a058ff5909412350d1c9a2904
GatoArtStudio <hackhcctecno@gmail.com>
2026-09-03 00:46:13 -0500

    add: The initial migration was added to create the schema.

---
+++ Kradis/Migrations/20260903054454_InitialGuild.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kradis.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialGuild : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "guilds",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "binary(36)", nullable: false),
+                    GuildId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    AntiSpamChannelId = table.Column<ulong>(type: "bigint unsigned", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_guilds", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_guilds_GuildId",
+                table: "guilds",
+                column: "GuildId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "guilds");
+        }
+    }
+}
